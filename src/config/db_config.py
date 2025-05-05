038821be2c698577f304bdb984dfbc9783bae899
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-04 21:31:56 -0600

    remover validacion de conexión a la base de datos

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -6,16 +6,3 @@ PATH_SERVICE_ACCOUNT = "credentials.json"
 cred = credentials.Certificate(PATH_SERVICE_ACCOUNT)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
-
-# validad conexion
-def validate_connection():
-    try:
-        test_ref = db.collection("test_collection").limit(1).get()
-        if test_ref:
-            print("Connection to Firestore is successful.")
-        else:
-            print("Connection to Firestore failed.")
-    except Exception as e:
-        print(f"Error connecting to Firestore: {e}")
-
-validate_connection()
