391999f6aebf1d429a274951d1bbe443d9ebbb8d
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-04 21:49:33 -0600

    refactorizar esquemas para ``Oferta``

--- src/models/schemas/oferta.py
+++ src/models/schemas/oferta.py
@@ -6,14 +6,23 @@ from .modalidad import Modalidad
 from .busqueda import Busqueda
 from .pagina import Pagina
 
-class Oferta(BaseModel):
-    # id: Any = Field(default=None)
+class OfertaCreate(BaseModel):
+    titulo:str
+    descrcipcion:str
+    salario: float
+    publidado: date
+    url: str
+    # TODO: agregar parametros extra al implementar el controlador
+
+
+class OfertaLinked(BaseModel):
+    id: str
     titulo: str = Field(min_length=1)
     descripcion: str
     empresa: Optional[Empresa] = None
     modalidad: Optional[Modalidad] = None
     salario: float
     publicado: date
-    pagiana: Optional[Pagina] = None
+    pagina: Optional[Pagina] = None
     busqueda: Optional[Busqueda] = None
     url: str
