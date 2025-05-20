d989f6df09d85640d687ad139ef387c3b1d3dfbf
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-20 16:03:29 -0600

    cargar credenciales de firebase desde variables de entorno

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -1,8 +1,22 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+import dotenv
 
-# Initialize Firebase Admin SDK
-PATH_SERVICE_ACCOUNT = "credentials.json"
-cred = credentials.Certificate(PATH_SERVICE_ACCOUNT)
+dotenv.load_dotenv()
+
+firebase_creds = {
+    "type": os.environ["FIREBASE_TYPE"],
+    "project_id": os.environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": os.environ["FIREBASE_AUTH_URI"],
+    "token_uri": os.environ["FIREBASE_TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["FIREBASE_AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"],
+}
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
