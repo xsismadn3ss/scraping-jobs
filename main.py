6badfa98be2826803cf6c62f09fb21fd22c4c829
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-20 18:03:47 -0600

    implement main function to run FastAPI application with Uvicorn

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import uvicorn
+
+
+def main():
+    uvicorn.run(
+        app="src.app:app", 
+        host="localhost", 
+        port=8000, 
+        reload=True
+    )
+
+
 if __name__ == "__main__":
-    pass
+    main()
