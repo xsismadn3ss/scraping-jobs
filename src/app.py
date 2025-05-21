9e0816e0a41f3741be89bc632730f6f955d42e13
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-05-20 18:15:55 -0600

    Refactor middleware setup for improved organization and clarity

--- src/app.py
+++ src/app.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from .routes import ofertas
+import os
 
 app = FastAPI()
 
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=[
+        os.environ["ALLOWED_HOST"],
+        "localhost",
+        "127.0.0..1",
+        "::1",
+    ],
+)
+
+
 @app.get("/")
 def read_root():
-    return {
-        "message": "Bienvenido a Sivar Chamba API"
-    }
+    return {"message": "Bienvenido a Sivar Chamba API"}
+
 
 app.include_router(ofertas.router)
